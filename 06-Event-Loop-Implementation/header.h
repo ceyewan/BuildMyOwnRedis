a6741747e62a1a2f60a2332ff43976a0939dcdc4
ceyewan <ceyewan@gmail.com>
2023-03-31 23:56:35 +0800

    add 06-Event-Loop-Implementation

---
+++ 06-Event-Loop-Implementation/header.h
@@ -0,0 +1,203 @@
+#include <arpa/inet.h>
+#include <assert.h>
+#include <cassert>
+#include <cstddef>
+#include <errno.h>
+#include <fcntl.h>
+#include <netinet/ip.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/poll.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <vector>
+
+#define LOG_ERROR(x) printf("[LOG_ERROR]: %s\n", x);
+#define LOG_INFO(x) printf("[LOG_INFO]: %s\n", x);
+
+const size_t k_max_msg = 4096;
+
+enum { STATE_REQ = 0, STATE_RES = 1, STATE_END = 2 };
+
+struct Conn {
+  int fd = -1;
+  uint32_t state = 0;
+  size_t rbuf_size = 0;
+  uint8_t rbuf[4 + k_max_msg];
+  size_t wbuf_size = 0;
+  size_t wbuf_sent = 0;
+  uint8_t wbuf[4 + k_max_msg];
+};
+
+static void state_req(Conn *conn);
+static void state_res(Conn *conn);
+
+static void fd_set_nb(int fd) {
+  errno = 0;
+  int flags = fcntl(fd, F_GETFL, 0);
+  if (errno) {
+    LOG_ERROR("fcntl error!");
+    return;
+  }
+  flags |= O_NONBLOCK;
+  fcntl(fd, F_SETFL, flags);
+  if (errno) {
+    LOG_ERROR("fcntl error!");
+    return;
+  }
+}
+
+static bool try_one_request(Conn *conn) {
+  if (conn->rbuf_size < 4) {
+    return false;
+  }
+  uint32_t len = 0;
+  memcpy(&len, &conn->rbuf[0], 4);
+  if (len > k_max_msg) {
+    LOG_INFO("too long");
+    conn->state = STATE_END;
+    return false;
+  }
+  if (4 + len > conn->rbuf_size) {
+    // not enough data in the buffer. Will retry in the next iteration
+    return false;
+  }
+
+  // got one request, do something with it
+  printf("client says: %.*s\n", len, &conn->rbuf[4]);
+
+  // generating echoing response
+  memcpy(&conn->wbuf[0], &len, 4);
+  memcpy(&conn->wbuf[4], &conn->rbuf[4], len);
+  conn->wbuf_size = 4 + len;
+
+  // remove the request from the buffer.
+  // note: frequent memmove is inefficient.
+  // note: need better handling for production code.
+  size_t remain = conn->rbuf_size - 4 - len;
+  if (remain) {
+    memmove(conn->rbuf, &conn->rbuf[4 + len], remain);
+  }
+  conn->rbuf_size = remain;
+
+  // change state
+  conn->state = STATE_RES;
+  state_res(conn);
+  // continue the outer loop if the request was fully processed
+  return (conn->state == STATE_REQ);
+}
+
+static bool try_fill_buffer(Conn *conn) {
+  assert(conn->rbuf_size < sizeof(conn->rbuf));
+  ssize_t ret = 0;
+  do {
+    size_t cap = sizeof(conn->rbuf) - conn->rbuf_size;
+    ret = read(conn->fd, &conn->rbuf[conn->rbuf_size], cap);
+  } while (ret < 0 && errno == EINTR);
+  if (ret < 0 && errno == EAGAIN) {
+    return false;
+  }
+  if (ret < 0) {
+    LOG_ERROR("read error");
+    conn->state = STATE_END;
+    return false;
+  }
+  if (ret == 0) {
+    if (conn->rbuf_size > 0) {
+      LOG_INFO("unexpected EOF");
+    } else {
+      LOG_INFO("EOF");
+    }
+    conn->state = STATE_END;
+    return false;
+  }
+  conn->rbuf_size += (size_t)ret;
+  assert(conn->rbuf_size <= sizeof(conn->rbuf) - conn->rbuf_size);
+  while (try_one_request(conn)) {
+  }
+  return conn->state == STATE_REQ;
+}
+
+static void state_req(Conn *conn) {
+  while (try_fill_buffer(conn)) {
+  }
+}
+
+static bool try_flush_buffer(Conn *conn) {
+  ssize_t ret = 0;
+  do {
+    size_t remain = conn->wbuf_size - conn->wbuf_sent;
+    ret = write(conn->fd, &conn->wbuf[conn->wbuf_sent], remain);
+  } while (ret < 0 && errno == EINTR);
+  if (ret < 0 && errno == EAGAIN) {
+    return false;
+  }
+  if (ret < 0) {
+    LOG_ERROR("write error");
+    conn->state = STATE_END;
+    return false;
+  }
+  conn->wbuf_sent += (size_t)ret;
+  assert(conn->wbuf_sent <= conn->wbuf_size);
+
+  if (conn->wbuf_sent == conn->wbuf_size) {
+    conn->state = STATE_REQ;
+    conn->wbuf_sent = 0;
+    conn->wbuf_size = 0;
+    return false;
+  }
+  return true;
+}
+
+static void state_res(Conn *conn) {
+  while (try_flush_buffer(conn)) {
+  }
+}
+
+static void connection_io(Conn *conn) {
+  if (conn->state == STATE_REQ) {
+    state_req(conn);
+  } else if (conn->state == STATE_RES) {
+    state_res(conn);
+  } else {
+    assert(0);
+  }
+}
+
+static void conn_put(std::vector<Conn *> &fd2conn, struct Conn *conn) {
+  if (fd2conn.size() <= (size_t)conn->fd) {
+    fd2conn.resize(conn->fd + 1);
+  }
+  fd2conn[conn->fd] = conn;
+}
+
+static int32_t accept_new_conn(std::vector<Conn *> &fd2conn, int fd) {
+  // accept
+  struct sockaddr_in client_addr = {};
+  socklen_t socklen = sizeof(client_addr);
+  int connfd = accept(fd, (struct sockaddr *)&client_addr, &socklen);
+  if (connfd < 0) {
+    LOG_INFO("accept() error");
+    return -1; // error
+  }
+
+  // set the new connection fd to nonblocking mode
+  fd_set_nb(connfd);
+  // creating the struct Conn
+  // struct Conn *conn = (struct Conn *)malloc(sizeof(struct Conn));
+  struct Conn *conn = new (struct Conn);
+  if (!conn) {
+    close(connfd);
+    return -1;
+  }
+  conn->fd = connfd;
+  conn->state = STATE_REQ;
+  conn->rbuf_size = 0;
+  conn->wbuf_size = 0;
+  conn->wbuf_sent = 0;
+  conn_put(fd2conn, conn);
+  return 0;
+}
---
+++ 06-Event-Loop-Implementation/server.cpp
@@ -0,0 +1,62 @@
+#include "header.h"
+
+int main() {
+  int fd = socket(AF_INET, SOCK_STREAM, 0);
+  assert(fd > 0);
+  int val = 1;
+  setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+  /* SO_REUSEADDR 允许在使用 bind 函数绑定端口之前
+   * 重新使用处于 TIME_WAIT 状态的端口*/
+  struct sockaddr_in addr = {};
+  addr.sin_family = AF_INET;
+  addr.sin_port = ntohs(1234);
+  addr.sin_addr.s_addr = ntohl(0); // 0.0.0.0
+  int ret = bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
+  assert(ret == 0);
+  ret = listen(fd, SOMAXCONN);
+  assert(ret == 0);
+  fd_set_nb(fd);
+
+  std::vector<Conn *> fd2conn;
+
+  // the event loop
+  std::vector<struct pollfd> poll_args;
+
+  while (true) {
+    poll_args.clear();
+    struct pollfd pfd = {fd, POLLIN, 0};
+    poll_args.push_back(pfd);
+
+    for (Conn *conn : fd2conn) {
+      if (!conn)
+        continue;
+      struct pollfd pfd = {};
+      pfd.fd = conn->fd;
+      pfd.events = (conn->state == STATE_REQ) ? POLLIN : POLLOUT;
+      pfd.events = pfd.events | POLLERR;
+      poll_args.push_back(pfd);
+    }
+
+    int ret = poll(poll_args.data(), (nfds_t)poll_args.size(), 1000);
+    if (ret < 0) {
+      LOG_ERROR("poll error!");
+      return -1;
+    }
+
+    for (size_t i = 1; i < poll_args.size(); i++) {
+      if (poll_args[i].revents) {
+        Conn *conn = fd2conn[poll_args[i].fd];
+        connection_io(conn);
+        if (conn->state == STATE_END) {
+          fd2conn[conn->fd] = NULL;
+          close(conn->fd);
+          free(conn);
+        }
+      }
+    }
+    if (poll_args[0].revents) {
+      accept_new_conn(fd2conn, fd);
+    }
+  }
+  return 0;
+}
